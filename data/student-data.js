aea6453d8c41722938f5749de4e1df7ddfe19045
jessikim <jessica.kim17@gmail.com>
2022-09-19 20:59:11 -0500

    add student-data file

---
+++ data/student-data.js
@@ -0,0 +1,19 @@
+const students = [
+  {name: 'Alan Powers', animal: 'bear', attendance: true},
+  {name: 'Alex Davidson', animal: 'rabbit', attendance: true},
+  {name: 'Arthur Read', animal: 'aardvark', attendance: true},
+  {name: 'Binky Barnes', animal: 'bulldog', attendance: false},
+  {name: 'Buster Baxter', animal: 'rabbit', attendance: true},
+  {name: 'Fern Walters', animal: 'dog', attendance: true},
+  {name: 'Francine Frensky', animal: 'monkey', attendance: true},
+  {name: 'George Lundgren', animal: 'moose', attendance: false},
+  {name: 'Jenna Morgan', animal: 'cat', attendance: true},
+  {name: 'Ladonna Compson', animal: 'rabbit', attendance: true},
+  {name: 'Maria Pappas', animal: 'rabbit', attendance: false},
+  {name: 'Muffy Crosswire', animal: 'monkey', attendance: true},
+  {name: 'Sue Ellen Armstrong', animal: 'cat', attendance: true}
+]
+
+export {
+  students
+}
